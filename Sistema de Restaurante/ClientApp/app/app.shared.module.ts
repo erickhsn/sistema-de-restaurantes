24e752903fc25542fb39b783d627b3962f6a5c6d
Erick Nunes <erickhsnunes@gmail.com>
2018-06-07 00:18:18 -0300

    Configurado opção de POST, com alguns bugs, mas funcional

--- Sistema de Restaurante/ClientApp/app/app.shared.module.ts
+++ Sistema de Restaurante/ClientApp/app/app.shared.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
@@ -28,6 +28,7 @@ import { CadastroRestauranteComponent } from './components/cadastro/restaurante/
         CommonModule,
         HttpModule,
         FormsModule,
+        ReactiveFormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
--- Sistema de Restaurante/ClientApp/app/components/cadastro/prato/cadastro.prato.component.ts
+++ Sistema de Restaurante/ClientApp/app/components/cadastro/prato/cadastro.prato.component.ts
@@ -1,8 +1,43 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'; 
+import { Http, RequestOptions } from '@angular/http';
+import { inject } from '@angular/core/testing';
 
 @Component({
     selector: 'cadastro-prato',
     templateUrl: './cadastro.prato.component.html'
 })
-export class CadastroPratoComponent {
+export class CadastroPratoComponent implements OnInit {
+
+    public pratoForm: FormGroup | undefined;
+    baseUrl: string = "";
+
+    constructor(private http: Http, @Inject('BASE_URL') _baseUrl: string, private fb: FormBuilder) {
+        this.baseUrl = _baseUrl;
+    }  
+
+    ngOnInit() {
+        this.pratoForm = this.fb.group({
+            nome: ['', [Validators.required]],
+            preco: ['', [Validators.required]]
+        });
+    }
+
+   /* constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + '/api/restaurante').subscribe(result => {
+            this.pratos = result.json() as Prato[];
+        }, error => console.error(error));
+    }*/
+
+    save() {
+        console.log(this.pratoForm);
+
+        let body = JSON.stringify(this.pratoForm);
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers : null });
+
+        return this.http.post(this.baseUrl + '/api/restaurante', body, options).subscribe();
+    }
+
+
 }
