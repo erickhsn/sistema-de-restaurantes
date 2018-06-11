021406aee90f4d705483f7d59e97976eacbdf1c7
Erick Nunes <erick.nunes@cedrotech.com>
2018-06-11 12:03:43 -0300

    Algumas correções de layout

--- Sistema de Restaurantes/ClientApp/app/components/app/app.component.ts
+++ Sistema de Restaurantes/ClientApp/app/components/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styles: ['footer {display: table; text-align: center; margin-left: auto; margin-right: auto; }']
 })
 export class AppComponent {
 }
